decf77f015204c539223dc8b37e1dfd2b27b1116
unknown <sandro007@gmail.com>
2024-01-24 13:42:39 -0300

    ok

--- VIEWS.R
+++ VIEWS.R
@@ -7,7 +7,8 @@ library(lubridate)
 con2 <- dbConnect(odbc::odbc(), "repro",encoding="Latin1")
 
 
-## RESULT
+## RESULT ==============================================
+
 
 result <- dbGetQuery(con2, statement = read_file('SQL/RESULT.sql'))
 
@@ -15,9 +16,20 @@ View(result)
 
 result %>% group_by(ANO,MES) %>% tally() %>% View()
 
-## PILARES
 
-pilares <- dbGetQuery(con2, statement = read_file('PILARES.sql'))
+result2 <- dbGetQuery(con2, statement = read_file('SQL/RESULT2.sql'))
+
+View(result2)
+
+result %>% group_by(ANO,MES) %>% tally() %>% View()
+
+
+
+
+## PILARES ===============================================
+
+
+pilares <- dbGetQuery(con2, statement = read_file('SQL/PILARES.sql'))
 
 View(pilares)
 
@@ -27,27 +39,31 @@ pilares %>%
   summarize(v=sum(VRVENDA))
 
 
-# DESCONTOS
+# DESCONTOS =================================
+
 
 descontos <- dbGetQuery(con2, statement = read_file('SQL/DESCONTOS.sql'))
 
 View(descontos)
 
 
-# CLIEN
+# CLIEN =======================================
 
-cliendesc <- dbGetQuery(con2, statement = read_file('DESCONTO_GERAL.sql'))
+
+cliendesc <- dbGetQuery(con2, statement = read_file('SQL/DESCONTO_GERAL.sql'))
 
 View(cliendesc)
 
-# VLXESPECIALISTA
+
+
+# VLXESPECIALISTA ====================================
 
 
 vlx_esp <- dbGetQuery(con2, statement = read_file('SQL/VLX_ESPECIAL.sql'))
 
 View(vlx_esp)
 
-# STATUSCLI
+# STATUSCLI ==============================================
 
 
 statuscli <- dbGetQuery(con2, statement = read_file('SQL/STATUSCLI.sql'))
@@ -55,7 +71,7 @@ statuscli <- dbGetQuery(con2, statement = read_file('SQL/STATUSCLI.sql'))
 View(statuscli)
 
 
-# BLOQUEIO
+# BLOQUEIO ======================================================
 
 
 bloqueio <- dbGetQuery(con2, statement = read_file('SQL/BLOQUEIO.sql'))
@@ -63,10 +79,49 @@ bloqueio <- dbGetQuery(con2, statement = read_file('SQL/BLOQUEIO.sql'))
 View(bloqueio)
 
 
+bloqueio2 <- dbGetQuery(con2, statement = read_file('SQL/BLOQUEIO2.sql'))
 
-# PACOTES
+View(bloqueio2)
+
+
+contasareceber <- dbGetQuery(con2, statement = read_file('SQL/REL_399_CONTASARECEBER.sql'))
+
+View(contasareceber)
+
+
+
+# PACOTES ====================================================================
 
 pacotes <- dbGetQuery(con2, statement = read_file('SQL/PACOTES.sql'))
 
 View(pacotes)
+
+
+write.csv2(pacotes,file = "C:\\Users\\REPRO SANDRO\\Documents\\R\\DASHBOARD_COMERCIAL\\BASES\\pacotes.csv",row.names = FALSE,na="")
+
+
+# ANALISE DESCTOS MCLIENTE =====================================================
+
+
+acordos_mclientes <- dbGetQuery(con2, statement = read_file('SQL/ANALISE_ACORDOS_MCLIENTES.sql'))
+
+View(acordos_mclientes)
+
+
+acordos_mclientes2 <- dbGetQuery(con2, statement = read_file('SQL/ANALISE_ACORDOS_MCLIENTES_2.sql'))
+
+View(acordos_mclientes2)
+
+
+acordos_mclientes3 <- 
+inner_join(acordos_mclientes,acordos_mclientes2,by="TBPCODIGO")
+
+View(acordos_mclientes3)
+
+write.csv2(acordos_mclientes3,file = "C:\\Users\\REPRO SANDRO\\Documents\\R\\DASHBOARD_COMERCIAL\\BASES\\acordos_mclientes.csv",row.names = FALSE,na="")
+
+
+dbGetQuery(con2,"SELECT * FROM CLITBP WHERE TBPCODIGO=307") %>% 
+  inner_join(.,acordos_mclientes2,by="TBPCODIGO") %>% 
+write.csv2(.,file = "C:\\Users\\REPRO SANDRO\\Documents\\R\\DASHBOARD_COMERCIAL\\BASES\\acordos_mclientes_relrepro.csv",row.names = FALSE,na="")
 
